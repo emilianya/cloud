cc865dc0ffdcf68c99916fd7b98839c1adbe5117
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-22 19:26:49 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 mongoose.connect(process.env.MONGODB_HOST);
+const crypto = require("crypto");
 //const nodemailer = require("nodemailer");
 //deal with email later
 
