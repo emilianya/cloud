97a3770d3623c18f9b23f31d967a052fed59584f
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-25 14:58:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -197,11 +197,12 @@ app.get('/delete', checkAuth, function(req,res) {
 })
 
 app.get('/upload', (req, res) => {
+	console.log(req.headers["host"])
 	res.render(__dirname + "/public/upload.ejs")
 })
 
 app.post('/upload', checkUploadAuth, upload.any(), async (req, res) => {
-	console.log("here")
+	console.log(req.headers["host"])
 	let many = false
 	let manyArray = []
 	if(req.files.length > 1) many = true
