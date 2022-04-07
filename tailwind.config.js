62c4cb61ab65901318f08ef7fd21b583c3c3513f
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-04-07 17:29:03 +0300

    darkmode testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   "./public/resources/*.js",
   './node_modules/tw-elements/dist/js/**/*.js'
   ],
-  darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
