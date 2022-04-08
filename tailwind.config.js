90255b51d97737f2a8de02a108575cffcfe9a313
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-04-08 15:37:57 +0300

    display the settings menu on small screen space devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
   theme: {
     screens: {
       'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
     },
     extend: {
       colors: {
